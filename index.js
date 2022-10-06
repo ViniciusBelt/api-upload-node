00a7b27f2010581ac6bd6dcec8dcd0b089340bc8
ViniciusBelt <super.vi@hotmail.com>
2022-10-06 08:32:06 -0300

    Selecionando pasta por params

--- index.js
+++ index.js
@@ -6,36 +6,36 @@ const fs = require('fs');
 let app = express();
 app.use(cors());
 
-const campanha = ''
+let campanha = 'images'
 
-function storage(campanha){
-  console.log(campanha)
-  const storage = multer.diskStorage ({
-    destination: function(req, file, cb) {
-      if(!fs.existsSync(`../${campanha} /`)) {
-        fs.mkdirSync(`../${campanha} /`)
-        cb(null, `../${campanha} /`)
-      } else {
-        cb(null, `../${campanha} /`)
-      }
-    },
-    filename: function(req, file, cb) {
-      cb(null, file.originalname)
+let storage = multer.diskStorage ({
+  destination: function(req, file, cb) {
+    if(!fs.existsSync(`../${campanha}/`)) {
+      fs.mkdirSync(`../${campanha}/`)
+      cb(null, `../${campanha}/`)
+    } else {
+      cb(null, `../${campanha}/`)
     }
-  })
-  
-  const upload = multer({storage})
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
 
-  return upload.array('file')
-}
+let upload = multer({storage})
 
 app.get('/', async (req, res) => {
   res.send({ running: true })
 });
 
-app.post('/upload', storage(1), async (req, res) => {
-  console.log(req.body.campanha)
-  this.campanha = req.body.campanha
+app.get('/campanha', async (req, res) => {
+  console.log(`Campanha selecionada: ${req.query.campanha}`);
+  campanha = campanha != req.query.campanha ? req.query.campanha : campanha
+  res.send({ campanha: campanha })
+});
+
+app.post('/upload', upload.array('file'), async (req, res) => {
+  campanha = 'images'
   res.send({ upload: true });
 });
 
